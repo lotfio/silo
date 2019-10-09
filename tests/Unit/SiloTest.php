52ec5fb5f90afc2a9b43cd163b7e3a628c5d692d
lotfio <contact@lotfio.net>
2019-10-09 22:00:13 +0100

    adding unit testing

---
+++ tests/Unit/SiloTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+/**
+ * @author    <contact@lotfio.net>
+ * @package   Silo PHP framework
+ * @version   1.0.0
+ * @license   MIT
+ * @category  Framework
+ * @copyright 2019 Lotfio Lakehal
+ *
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class SiloTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(TRUE);
+    }
+}
