f20d6738a37a1813e8ff502c6e326eff0d959f49
lotfio <contact@lotfio.net>
2019-10-11 14:07:56 +0100

    wip

---
+++ app/Models/Users.php
@@ -0,0 +1,16 @@
+<?php namespace App\Models;
+
+/**
+ * @author    <contact@lotfio.net>
+ * @package   Silo PHP framework
+ * @version   1.0.0
+ * @license   MIT
+ * @category  Framework
+ * @copyright 2019 Lotfio Lakehal
+ *
+ */
+
+class Users
+{
+
+}
